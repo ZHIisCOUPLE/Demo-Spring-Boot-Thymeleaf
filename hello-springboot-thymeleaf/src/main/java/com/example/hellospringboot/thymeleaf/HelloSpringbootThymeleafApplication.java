8807360908f50c0412a156e0c0bbca2ba07079f4
ZHIisCOUPLE <1796854053@qq.com>
2018-11-27 10:20:59 +0800

    SpringBoot整合Thymeleaf

---
+++ hello-springboot-thymeleaf/src/main/java/com/example/hellospringboot/thymeleaf/HelloSpringbootThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellospringboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringbootThymeleafApplication.class, args);
+    }
+}
